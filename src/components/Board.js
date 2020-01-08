ab49582b9978b6b79bf6c2a73a03c39a3fd09cf6
Theresa Ulwahn <theresaulwahn@gmail.com>
2020-01-08 12:28:23 +0100

    game codealong done

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 import { Square } from 'components/Square'
 
 export const Board = () => {
-  // TODO - use the 'squares' state from the game reducer
-  const squares = []
+  // This function takes the entire store from Redux and returns smth from the store. 
+  // This is an implicit return, that returns the array of squars
+  const squares = useSelector((store ) => store.game.squares)
+  
 
   return (
     <div className="board">
--- src/components/Square.js
+++ src/components/Square.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
+import { game } from 'reducers/game'
 import { Player } from 'components/Player'
 
 export const Square = ({ value, index }) => {
+  // down below helps us and allows us to trigger things in our store
+  // within our redux store we can dispatch actions
+  const dispatch = useDispatch()
+  
   const handleClick = () => {
-    // TODO send the captureSquare action
+    // this tells redux we have clicked on a square and now time to execute the action
+   // this is a payload
+    dispatch(game.actions.captureSquare({index}))
+    // Dispached the action above
   }
 
   return (
